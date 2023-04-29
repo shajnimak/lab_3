e30001430858a8d212d15fbfd25ec68864ed81fd
shadyman <m.m.shdmn@gmail.com>
2023-04-29 16:53:52 +0600

    documentation

--- lab3/src/MyArrayListStack.java
+++ lab3/src/MyArrayListStack.java
@@ -6,24 +6,49 @@ public class MyArrayListStack<T> {
     public MyArrayListStack(MyArrayList<T> arrayList) {
         this.arrayList = arrayList;
     }
+
+    /**
+     * @push - method that adds an element to the top of the stack
+     * @param element - Object
+     */
     public void push(Object element){
         arrayList.add(element, 0);
     }
+
+    /**
+     * @pop - remethod that removes and returns the top element of the stack
+     * @return - removed element
+     */
     public Object pop(){
         if (isEmpty()) {
             throw new EmptyStackException();
         }
         return arrayList.remove(0);
     }
+
+    /**
+     * @peek - gmethod that returns the top element of the stack without removing it
+     * @return - top element of stack
+     */
     public Object peek(){
         if (isEmpty()) {
             throw new EmptyStackException();
         }
         return arrayList.get(0);
     }
+
+    /**
+     * @isEmpty - method that returns true if the stack is empty or false if it is not
+     * @return - true/false
+     */
     public boolean isEmpty(){
         return arrayList.isEmpty();
     }
+
+    /**
+     * @size -  method that returns the number of elements in the stack
+     * @return - size(int)
+     */
     public int size(){
         return arrayList.size();
     }
