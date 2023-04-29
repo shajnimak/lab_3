509030ce860b7a6efcc01eef6339b793c66f2a93
shadyman <m.m.shdmn@gmail.com>
2023-04-29 15:57:41 +0600

    Constructor MyArrayListStack

--- lab3/src/MyArrayListStack.java
+++ lab3/src/MyArrayListStack.java
@@ -3,6 +3,9 @@ import service.MyArrayList;
 public class MyArrayListStack {
     MyArrayList arrayList = new MyArrayList();
 
+    public MyArrayListStack(MyArrayList arrayList) {
+        this.arrayList = arrayList;
+    }
 
 
 }
